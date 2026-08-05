bfe283f00b84f2d31a40e872f6963903f68e8419
Hai Nguyen <hnguyen@northern.co>
2026-08-05 08:33:11 +0700

    account for baseurl in _config.yml to fix broken links in Github Page

--- _plugins/wikilinks.rb
+++ _plugins/wikilinks.rb
@@ -1,12 +1,15 @@
 module Jekyll
   module WikilinkFilter
     def self.convert(content, site)
+      baseurl = site.config['baseurl'] || ''
+
       content.gsub(/\[\[([^\]]+)\]\]/) do
         slug = $1.strip
         target = site.posts.docs.find { |doc| doc.basename_without_ext == slug }
 
         if target
-          "<a href=\"#{target.url}\" class=\"wikilink\">#{target.data['title'] || slug}</a>"
+          full_url = File.join(baseurl, target.url)
+          "<a href=\"#{full_url}\" class=\"wikilink\">#{target.data['title'] || slug}</a>"
         else
           "<span class=\"wikilink-missing\" title=\"Note not found: #{slug}\">#{slug}</span>"
         end
